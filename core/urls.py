ab9ac472c0d1b51ca674137d33ea33c76bff4c7c
Breno <brenoramon55@gmail.com>
2024-12-13 03:16:16 -0300

    feat(deploy): implement deploy render (Usuário: Breno)

--- core/urls.py
+++ core/urls.py
@@ -3,11 +3,14 @@ from django.urls import path
 from core.api import api
 from django.conf.urls.static import static
 from django.conf import settings
+from django.http import HttpResponse  # Adicionada a importação para HttpResponse
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', api.urls),  # Certifique-se de que só está registrado aqui
+    path('api/v1/', api.urls),  
+    path('', lambda request: HttpResponse("API is running!", content_type="text/plain")),
+
 ]
 
 if settings.DEBUG:
