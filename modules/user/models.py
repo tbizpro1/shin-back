4af2e3dc09a7b94dd9f0df9c4393616ae369cef8
Breno <brenoramon55@gmail.com>
2025-02-12 18:33:12 -0300

    fix(bd): refactor schemas and models user (Usuário: Breno)

--- modules/user/models.py
+++ modules/user/models.py
@@ -1,3 +1,96 @@
+# from django.db import models
+# from django.contrib.auth.models import AbstractUser
+
+# class User(AbstractUser):
+#     ROLE_CHOICES = (
+#         ('admin', 'Admin'),
+#         ('user', 'User'),
+#     )
+    
+#     GENDER_CHOICES = (
+#     ('m', 'Male'),
+#     ('f', 'Female'),
+#     ('n', 'Prefer not to say'),
+#     )
+#     class state_choices(models.TextChoices):
+#         AC = "AC", "Acre"
+#         AL = "AL", "Alagoas"
+#         AP = "AP", "Amapá"
+#         AM = "AM", "Amazonas"
+#         BA = "BA", "Bahia"
+#         CE = "CE", "Ceará"
+#         DF = "DF", "Distrito Federal"
+#         ES = "ES", "Espírito Santo"
+#         GO = "GO", "Goiás"
+#         MA = "MA", "Maranhão"
+#         MT = "MT", "Mato Grosso"
+#         MS = "MS", "Mato Grosso do Sul"
+#         MG = "MG", "Minas Gerais"
+#         PA = "PA", "Pará"
+#         PB = "PB", "Paraíba"
+#         PR = "PR", "Paraná"
+#         PE = "PE", "Pernambuco"
+#         PI = "PI", "Piauí"
+#         RJ = "RJ", "Rio de Janeiro"
+#         RN = "RN", "Rio Grande do Norte"
+#         RS = "RS", "Rio Grande do Sul"
+#         RO = "RO", "Rondônia"
+#         RR = "RR", "Roraima"
+#         SC = "SC", "Santa Catarina"
+#         SP = "SP", "São Paulo"
+#         SE = "SE", "Sergipe"
+#         TO = "TO", "Tocantins"
+
+#     class EducationLevelChoices(models.TextChoices):
+#         SEM_ESCOLARIDADE = "Sem escolaridade", "Sem escolaridade"
+#         ENSINO_FUNDAMENTAL = "Ensino Fundamental", "Ensino Fundamental"
+#         ENSINO_MEDIO = "Ensino Médio", "Ensino Médio"
+#         GRADUACAO = "Graduação", "Graduação"
+#         ESPECIALIZACAO = "Especialização", "Especialização"
+#         MESTRADO = "Mestrado", "Mestrado"
+#         DOUTORADO = "Doutorado", "Doutorado"
+#         POS_DOUTORADO = "Pós-Doutorado", "Pós-Doutorado"
+    
+#     class ethnicity_choices(models.TextChoices):
+#         BRANCA = "Branca", "Branca"
+#         PRETA = "Preta", "Preta"
+#         PARDA = "Parda", "Parda"
+#         AMARELA = "Amarela", "Amarela"
+#         INDIGENA = "Indígena", "Indígena"
+
+#     id = models.AutoField(primary_key=True)
+#     username = models.CharField(max_length=50, unique=True)
+#     email = models.EmailField(max_length=80, unique=True)
+#     password = models.CharField(max_length=128)
+#     phone = models.CharField(max_length=11, null=True, blank=True, default='')
+#     linkedin = models.CharField(max_length=255, null=True, blank=True, default='')
+#     profession = models.CharField(max_length=128, null=True, blank=True, default='')
+#     date_joined = models.DateTimeField(auto_now_add=True)
+#     last_login = models.DateTimeField(auto_now=True)
+#     is_active = models.BooleanField(default=True)
+#     role = models.CharField(max_length=50, choices=ROLE_CHOICES, default='user', null=True, blank=True)
+#     profile_picture = models.URLField(max_length=255, null=True, blank=True)
+#     state = models.CharField(max_length=100,choices=state_choices.choices, null=True, blank=True)
+#     gender = models.CharField(max_length=1, choices=GENDER_CHOICES, null=True, blank=True)
+#     institution = models.CharField(max_length=255, null=True, blank=True)
+#     education_level = models.CharField(max_length=255,choices=EducationLevelChoices.choices, null=True, blank=True)
+#     ethnicity = models.CharField(max_length=255,choices=ethnicity_choices.choices, null=True, blank=True)
+#     city = models.CharField(max_length=100, null=True, blank=True)
+#     whatsapp_number = models.CharField(max_length=15, null=True, blank=True, default='')
+#     weekly_hours_worked = models.IntegerField(null=True, blank=True)
+#     date_of_birth = models.DateField(null=True, blank=True)
+#     country = models.CharField(max_length=100, null=True, blank=True)
+#     cep = models.CharField(max_length=20, null=True, blank=True)
+#     # street = models.CharField(max_length=255, null=True, blank=True)
+#     # neighborhood = models.CharField(max_length=255, null=True, blank=True)
+#     # number = models.CharField(max_length=10, null=True, blank=True)
+#     cpf = models.CharField(max_length=14, unique=True, null=True, blank=True)  # Novo campo CPF
+
+#     REQUIRED_FIELDS = ['username', 'password']
+#     USERNAME_FIELD = 'email'
+
+#     def __str__(self):
+#         return f'{self.username} - {self.email}'
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
@@ -68,17 +161,17 @@ class User(AbstractUser):
     date_joined = models.DateTimeField(auto_now_add=True)
     last_login = models.DateTimeField(auto_now=True)
     is_active = models.BooleanField(default=True)
-    role = models.CharField(max_length=50, choices=ROLE_CHOICES, default='user', null=True, blank=True)
+    role = models.CharField(max_length=50,choices=ROLE_CHOICES,  default='user', null=True, blank=True)
     profile_picture = models.URLField(max_length=255, null=True, blank=True)
-    state = models.CharField(max_length=100,choices=state_choices.choices, null=True, blank=True)
-    gender = models.CharField(max_length=1, choices=GENDER_CHOICES, null=True, blank=True)
+    state = models.CharField(max_length=100, null=True, blank=True)#choices=state_choices.choices,
+    gender = models.CharField(max_length=1, null=True, blank=True)# choices=GENDER_CHOICES,
     institution = models.CharField(max_length=255, null=True, blank=True)
-    education_level = models.CharField(max_length=255,choices=EducationLevelChoices.choices, null=True, blank=True)
-    ethnicity = models.CharField(max_length=255,choices=ethnicity_choices.choices, null=True, blank=True)
+    education_level = models.CharField(max_length=255, null=True, blank=True)#choices=EducationLevelChoices.choices,
+    ethnicity = models.CharField(max_length=255, null=True, blank=True)#choices=ethnicity_choices.choices,
     city = models.CharField(max_length=100, null=True, blank=True)
     whatsapp_number = models.CharField(max_length=15, null=True, blank=True, default='')
-    weekly_hours_worked = models.IntegerField(null=True, blank=True)
-    date_of_birth = models.DateField(null=True, blank=True)
+    # weekly_hours_worked = models.IntegerField(null=True, blank=True, default=0)
+    # date_of_birth = models.DateField(null=True, blank=True)
     country = models.CharField(max_length=100, null=True, blank=True)
     cep = models.CharField(max_length=20, null=True, blank=True)
     # street = models.CharField(max_length=255, null=True, blank=True)
--- modules/user/schemas.py
+++ modules/user/schemas.py
@@ -1,13 +1,226 @@
+# from enum import Enum
+# from typing import Optional
+# from ninja import Schema, Field
+# from datetime import datetime
+# from pydantic import validator
+# from datetime import datetime, date
+
+# from ..utils.validator_cpf import validar_cpf
+# class RoleFilterEnum(str, Enum):
+#     user = "user"
+
+# class StateEnum(str, Enum):
+#     AC = "AC"
+#     AL = "AL"
+#     AP = "AP"
+#     AM = "AM"
+#     BA = "BA"
+#     CE = "CE"
+#     DF = "DF"
+#     ES = "ES"
+#     GO = "GO"
+#     MA = "MA"
+#     MT = "MT"
+#     MS = "MS"
+#     MG = "MG"
+#     PA = "PA"
+#     PB = "PB"
+#     PR = "PR"
+#     PE = "PE"
+#     PI = "PI"
+#     RJ = "RJ"
+#     RN = "RN"
+#     RS = "RS"
+#     RO = "RO"
+#     RR = "RR"
+#     SC = "SC"
+#     SP = "SP"
+#     SE = "SE"
+#     TO = "TO"
+
+# class EducationLevelEnum(str, Enum):
+#     SEM_ESCOLARIDADE = "Sem escolaridade"
+#     ENSINO_FUNDAMENTAL = "Ensino Fundamental"
+#     ENSINO_MEDIO = "Ensino Médio"
+#     GRADUACAO = "Graduação"
+#     ESPECIALIZACAO = "Especialização"
+#     MESTRADO = "Mestrado"
+#     DOUTORADO = "Doutorado"
+#     POS_DOUTORADO = "Pós-Doutorado"
+
+# class GenderEnum(str, Enum):
+#     m = "m"
+#     f = "f"
+#     n = "p"
+
+# class EthnicityEnum(str, Enum):
+#     BRANCA = "Branca"
+#     PRETA = "Preta"
+#     PARDA = "Parda"
+#     AMARELA = "Amarela"
+#     INDIGENA = "Indígena"
+
+
+# class UserPostSchema(Schema):
+#     username: str
+#     email: str
+#     password: str
+#     phone: Optional[str] = None
+#     linkedin: Optional[str] = None
+#     profession: Optional[str] = None
+#     role: RoleFilterEnum
+#     state: StateEnum = None
+#     gender: GenderEnum = None
+#     institution: Optional[str] = None
+#     education_level: EducationLevelEnum = None
+#     ethnicity: EthnicityEnum = None
+#     city: Optional[str] = None
+#     whatsapp_number: Optional[str] = None
+#     weekly_hours_worked: Optional[int] = None
+#     date_of_birth: Optional[datetime] = None
+#     country: Optional[str] = None
+#     cep: Optional[str] = None
+#     # street: Optional[str] = None
+#     # neighborhood: Optional[str] = None
+#     # number: Optional[str] = None
+#     cpf: Optional[str] = None
+
+
+#     @validator("cpf")
+#     def validar_cpf(cls, cpf):
+#         if not validar_cpf(cpf):
+#             raise ValueError("CPF inválido")
+#         return cpf
+
+
+# class UserPutSchema(Schema):
+#     username: Optional[str] = Field(None, alias="username", title="Nome de usuário")
+#     email: Optional[str] = Field(None, alias="email", title="Email")
+#     password: Optional[str] = Field(None, alias="password", title="Senha")
+#     phone: Optional[str] = Field(None, alias="phone", title="Telefone")
+#     linkedin: Optional[str] = Field(None, alias="linkedin", title="LinkedIn")
+#     profession: Optional[str] = Field(None, alias="profession", title="Profissão")
+#     role: Optional[RoleFilterEnum] = None
+#     state: StateEnum = None
+#     gender: Optional[GenderEnum] = None
+#     institution: Optional[str] = None
+#     education_level: Optional[EducationLevelEnum] = None
+#     ethnicity: EthnicityEnum = None
+#     city: Optional[str] = None
+#     whatsapp_number: Optional[str] = None
+#     weekly_hours_worked: Optional[int] = None
+#     date_of_birth: Optional[datetime] = None
+#     country: Optional[str] = None
+#     cep: Optional[str] = None
+#     # street: Optional[str] = None
+#     # neighborhood: Optional[str] = None
+#     # number: Optional[str] = None
+#     cpf: Optional[str] = None
+
+
+# class UserListSchema(Schema):
+#     id: int
+#     username: str
+#     email: str
+#     phone: Optional[str] = None
+#     linkedin: Optional[str] = None
+#     profession: Optional[str] = None
+#     role: str
+#     profile_picture: Optional[str] = None
+#     state: StateEnum = None
+#     gender: Optional[GenderEnum] = None
+#     institution: Optional[str] = None
+#     education_level: Optional[EducationLevelEnum] = None
+#     ethnicity: EthnicityEnum = None
+#     city: Optional[str] = None
+#     whatsapp_number: Optional[str] = None
+#     weekly_hours_worked: Optional[int] = None
+#     date_of_birth: Optional[datetime] = None
+#     country: Optional[str] = None
+#     date_joined: datetime
+#     last_login: datetime
+#     age: Optional[int] = None  
+#     is_active: bool
+#     cep: Optional[str] = None
+#     # street: Optional[str] = None
+#     # neighborhood: Optional[str] = None
+#     # number: Optional[str] = None
+#     cpf: Optional[str] = None
+
+#     @validator("age", pre=True, always=True)
+#     def calcular_idade(cls, _, values):
+#         date_of_birth = values.get("date_of_birth")
+#         if date_of_birth:
+#             today = date.today()
+#             return (
+#                 today.year
+#                 - date_of_birth.year
+#                 - ((today.month, today.day) < (date_of_birth.month, date_of_birth.day))
+#             )
+#         return None
+
+# # Schema de resposta para registro
+# class RegisterSchema(Schema):
+#     username: str
+#     email: str
+#     password: str
+#     phone: Optional[str] = None
+#     linkedin: Optional[str] = None
+#     profession: Optional[str] = None
+#     role: RoleFilterEnum
+#     state: StateEnum = None
+#     gender: Optional[GenderEnum] = None
+#     institution: Optional[str] = None
+#     education_level: Optional[EducationLevelEnum] = None
+#     ethnicity: EthnicityEnum = None
+#     city: Optional[str] = None
+#     whatsapp_number: Optional[str] = None
+#     weekly_hours_worked: Optional[int] = None
+#     date_of_birth: Optional[datetime] = None
+#     country: Optional[str] = None
+#     cep: Optional[str] = None
+#     # street: Optional[str] = None
+#     # neighborhood: Optional[str] = None
+#     # number: Optional[str] = None
+#     cpf: Optional[str] = None
+
+
+# class RegisterResponseSchema(Schema):
+#     id: int
+#     username: str
+#     email: str
+#     phone: Optional[str] = None
+#     linkedin: Optional[str] = None
+#     profession: Optional[str] = None
+#     state: StateEnum = None
+#     gender: Optional[GenderEnum] = None
+#     institution: Optional[str] = None
+#     education_level: Optional[EducationLevelEnum] = None
+#     ethnicity: EthnicityEnum = None
+#     city: Optional[str] = None
+#     whatsapp_number: Optional[str] = None
+#     weekly_hours_worked: Optional[int] = None
+#     date_of_birth: Optional[datetime] = None
+#     country: Optional[str] = None
+#     cep: Optional[str] = None
+#     # street: Optional[str] = None
+#     # neighborhood: Optional[str] = None
+#     # number: Optional[str] = None
+#     cpf: Optional[str] = None
+
+
+# class ErrorResponse(Schema):
+#     message: str
 from enum import Enum
 from typing import Optional
 from ninja import Schema, Field
 from datetime import datetime
-from pydantic import validator
-from datetime import datetime, date
+from pydantic import validator,conint
 
-from ..utils.validator_cpf import validar_cpf
+
 class RoleFilterEnum(str, Enum):
     user = "user"
+
 
 class StateEnum(str, Enum):
     AC = "AC"
@@ -38,6 +251,7 @@ class StateEnum(str, Enum):
     SE = "SE"
     TO = "TO"
 
+
 class EducationLevelEnum(str, Enum):
     SEM_ESCOLARIDADE = "Sem escolaridade"
     ENSINO_FUNDAMENTAL = "Ensino Fundamental"
@@ -48,10 +262,12 @@ class EducationLevelEnum(str, Enum):
     DOUTORADO = "Doutorado"
     POS_DOUTORADO = "Pós-Doutorado"
 
+
 class GenderEnum(str, Enum):
     m = "m"
     f = "f"
     n = "p"
+
 
 class EthnicityEnum(str, Enum):
     BRANCA = "Branca"
@@ -68,53 +284,40 @@ class UserPostSchema(Schema):
     phone: Optional[str] = None
     linkedin: Optional[str] = None
     profession: Optional[str] = None
-    role: RoleFilterEnum
-    state: StateEnum = None
-    gender: GenderEnum = None
+    role: Optional[str] = None
+    state: Optional[str] = None
+    gender: Optional[str] = None
     institution: Optional[str] = None
-    education_level: EducationLevelEnum = None
-    ethnicity: EthnicityEnum = None
+    education_level: Optional[str] = None
+    ethnicity: Optional[str] = None
     city: Optional[str] = None
     whatsapp_number: Optional[str] = None
-    weekly_hours_worked: Optional[int] = None
-    date_of_birth: Optional[datetime] = None
+    # weekly_hours_worked: Optional[conint(ge=0)] = None
+    # date_of_birth: Optional[str] = None
     country: Optional[str] = None
     cep: Optional[str] = None
-    # street: Optional[str] = None
-    # neighborhood: Optional[str] = None
-    # number: Optional[str] = None
     cpf: Optional[str] = None
 
 
-    @validator("cpf")
-    def validar_cpf(cls, cpf):
-        if not validar_cpf(cpf):
-            raise ValueError("CPF inválido")
-        return cpf
-
-
 class UserPutSchema(Schema):
-    username: Optional[str] = Field(None, alias="username", title="Nome de usuário")
-    email: Optional[str] = Field(None, alias="email", title="Email")
-    password: Optional[str] = Field(None, alias="password", title="Senha")
-    phone: Optional[str] = Field(None, alias="phone", title="Telefone")
-    linkedin: Optional[str] = Field(None, alias="linkedin", title="LinkedIn")
-    profession: Optional[str] = Field(None, alias="profession", title="Profissão")
-    role: Optional[RoleFilterEnum] = None
-    state: StateEnum = None
-    gender: Optional[GenderEnum] = None
+    username: Optional[str] = None
+    email: Optional[str] = None
+    password: Optional[str] = None
+    phone: Optional[str] = None
+    linkedin: Optional[str] = None
+    profession: Optional[str] = None
+    role: Optional[str] = None
+    state: Optional[str] = None
+    gender: Optional[str] = None
     institution: Optional[str] = None
-    education_level: Optional[EducationLevelEnum] = None
-    ethnicity: EthnicityEnum = None
+    education_level: Optional[str] = None
+    ethnicity: Optional[str] = None
     city: Optional[str] = None
     whatsapp_number: Optional[str] = None
-    weekly_hours_worked: Optional[int] = None
-    date_of_birth: Optional[datetime] = None
+    # weekly_hours_worked: Optional[conint(ge=0)] = None
+    # date_of_birth: Optional[str] = None
     country: Optional[str] = None
     cep: Optional[str] = None
-    # street: Optional[str] = None
-    # neighborhood: Optional[str] = None
-    # number: Optional[str] = None
     cpf: Optional[str] = None
 
 
@@ -125,41 +328,25 @@ class UserListSchema(Schema):
     phone: Optional[str] = None
     linkedin: Optional[str] = None
     profession: Optional[str] = None
-    role: str
+    role: Optional[str] = None
     profile_picture: Optional[str] = None
-    state: StateEnum = None
-    gender: Optional[GenderEnum] = None
+    state: Optional[str] = None
+    gender: Optional[str] = None
     institution: Optional[str] = None
-    education_level: Optional[EducationLevelEnum] = None
-    ethnicity: EthnicityEnum = None
+    education_level: Optional[str] = None
+    ethnicity: Optional[str] = None
     city: Optional[str] = None
     whatsapp_number: Optional[str] = None
-    weekly_hours_worked: Optional[int] = None
-    date_of_birth: Optional[datetime] = None
+    # weekly_hours_worked: Optional[conint(ge=0)] = None
+    # date_of_birth: Optional[str] = None
     country: Optional[str] = None
     date_joined: datetime
     last_login: datetime
-    age: Optional[int] = None  
     is_active: bool
     cep: Optional[str] = None
-    # street: Optional[str] = None
-    # neighborhood: Optional[str] = None
-    # number: Optional[str] = None
     cpf: Optional[str] = None
 
-    @validator("age", pre=True, always=True)
-    def calcular_idade(cls, _, values):
-        date_of_birth = values.get("date_of_birth")
-        if date_of_birth:
-            today = date.today()
-            return (
-                today.year
-                - date_of_birth.year
-                - ((today.month, today.day) < (date_of_birth.month, date_of_birth.day))
-            )
-        return None
 
-# Schema de resposta para registro
 class RegisterSchema(Schema):
     username: str
     email: str
@@ -167,21 +354,18 @@ class RegisterSchema(Schema):
     phone: Optional[str] = None
     linkedin: Optional[str] = None
     profession: Optional[str] = None
-    role: RoleFilterEnum
-    state: StateEnum = None
-    gender: Optional[GenderEnum] = None
+    role: Optional[str] = None
+    state: Optional[str] = None
+    gender: Optional[str] = None
     institution: Optional[str] = None
-    education_level: Optional[EducationLevelEnum] = None
-    ethnicity: EthnicityEnum = None
+    education_level: Optional[str] = None
+    ethnicity: Optional[str] = None
     city: Optional[str] = None
     whatsapp_number: Optional[str] = None
-    weekly_hours_worked: Optional[int] = None
-    date_of_birth: Optional[datetime] = None
+    # weekly_hours_worked: Optional[conint(ge=0)] = None
+    # date_of_birth: Optional[str] = None
     country: Optional[str] = None
     cep: Optional[str] = None
-    # street: Optional[str] = None
-    # neighborhood: Optional[str] = None
-    # number: Optional[str] = None
     cpf: Optional[str] = None
 
 
@@ -192,20 +376,17 @@ class RegisterResponseSchema(Schema):
     phone: Optional[str] = None
     linkedin: Optional[str] = None
     profession: Optional[str] = None
-    state: StateEnum = None
-    gender: Optional[GenderEnum] = None
+    state: Optional[str] = None
+    gender: Optional[str] = None
     institution: Optional[str] = None
-    education_level: Optional[EducationLevelEnum] = None
-    ethnicity: EthnicityEnum = None
+    education_level: Optional[str] = None
+    ethnicity: Optional[str] = None
     city: Optional[str] = None
     whatsapp_number: Optional[str] = None
-    weekly_hours_worked: Optional[int] = None
-    date_of_birth: Optional[datetime] = None
+    # weekly_hours_worked: Optional[conint(ge=0)] = None
+    # date_of_birth: Optional[str] = None
     country: Optional[str] = None
     cep: Optional[str] = None
-    # street: Optional[str] = None
-    # neighborhood: Optional[str] = None
-    # number: Optional[str] = None
     cpf: Optional[str] = None
 
 
